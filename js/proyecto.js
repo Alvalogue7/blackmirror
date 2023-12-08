44370bc3c83614bf8ae930bbef3c794d0d30bd7c
Álvaro López <alvalogue7@gmail.com>
2023-12-08 16:09:47 +0100

    08/12/2023

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,3 +1,8 @@
+/***************************************************************************/
+/*                          author: Álvaro López                           */
+/*                          version: 2.0                                   */
+/*                          comments: class proyect 2023-34                */
+/***************************************************************************/
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
